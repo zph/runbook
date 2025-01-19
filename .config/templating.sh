420aaefb9c8f965d3077adf557bc8d936720ba40
Zander Hill <zander@xargs.io>
2025-01-19 12:01:22 -0800

    Update README

--- .config/templating.sh
+++ .config/templating.sh
@@ -3,5 +3,6 @@
 set -eou pipefail
 
 export RUNBOOK_HELP="$(runbook --help)"
+export RUNBOOK_VERSION="$(runbook version | grep -i version | awk '{print $3}')"
 
 envsubst < ".config/README.md.template" > README.md
